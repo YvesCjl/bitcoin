e8ca256db30f05551a1473bad8992465d256fda1
YvesCjl <YvesCjl@gmail.com>
2019-03-15 11:04:35 +0800

    successfully verified

--- verifier/main.py
+++ verifier/main.py
@@ -8,7 +8,7 @@ import os
 
 w3 = Web3( HTTPProvider( "http://127.0.0.1:8545" ) )
 w3.eth.defaultAccount = w3.eth.accounts[-2]
-contract = w3.eth.contract( address="0xa2a5a412BB5D333D1F98D5e330059082a9f51866", abi=json.load( open( "../contract/contracts/contract.abi", "r" ) ) )
+contract = w3.eth.contract( address="0x440CB52DA138b4003994A7Ff2BC8356e2B436E96", abi=json.load( open( "../contract/contracts/contract.abi", "r" ) ) )
 
 try:
     prkey = Account.decrypt( json.load( open( "./.keystore/key", "r" ) ), "123" )
@@ -19,25 +19,27 @@ except:
 account = Account.privateKeyToAccount( prkey )
 
 def verify( address, name ):
-    fhash = hashlib.sha256( ( account.address+name ).encode( "utf8" ) ).hexdigest()
+    fhash = hashlib.sha256( ( address+name ).encode( "utf8" ) ).hexdigest()
     path, chal = fhash[ :32 ], fhash[ 32: ]
-    with open( "../website/"+path, "rb" ) as fd:
+    try:
+        fd = open( "../website/"+path, "rb" )
         if w3.eth.account.recoverHash( defunct_hash_message( text=chal ), signature=fd.read() )==address:
             print( "Verify passed!" )
             return True
-    print( "Failed!" )
-    return False
+    except:
+        print( "Failed!" )
+        return False
 
-evefil = contract.events.reg.createFilter( 0 )
+evefil = contract.events.reg.createFilter( fromBlock=0 )
 while True: 
     for event in evefil.get_new_entries():
-        if verify( event.args[ "address" ], event.args[ "name" ] ):
+        if verify( event.args[ "addr" ], event.args[ "domainName" ] ):
             pass
         else:
             pass
+    time.sleep( 5 )
     #print( w3.eth.account.recoverHash( defunct_hash_message( text=chal ), signature=vcode.signature ) )
     #contract.functions.Test().call()
     #contract.functions.verify( account.address, name ).transact( transaction={ "gas":1145141919 } )
     #print( account.address )
 	
-#register( "www.baidu.com" )
